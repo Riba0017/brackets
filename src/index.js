41e6ff7db40a8abb17ea68247952aa73ff8f6c38
Kate <Riba0017@yandex.ru>
2020-10-15 20:54:45 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+    let brackets = bracketsConfig.flatMap(elem => {
+        return elem.join('');
+    });
+    let start, end;
+    do {
+        start = str.length;
+        for(let i = 0; i < brackets.length; i++) {
+            str = str.replace(brackets[i], '')
+        }
+        end = str.length;
+    } while(start > end);
+
+    return (str.length === 0)
+
+};
+
